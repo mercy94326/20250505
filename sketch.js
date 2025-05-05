77148a98e37e3f5f3b54c707eea82caba5521990
mercy94326 <mercy94326@gmail.com>
2025-05-05 10:17:33 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,23 +1,21 @@
-// Face Mesh Detection - Triangulated Face Mapping  
-// https://thecodingtrain.com/tracks/ml5js-beginners-guide/ml5/facemesh  
-// https://youtu.be/R5UZsIwPbJA  
+// Hand Pose Detection with ml5.js
+// https://thecodingtrain.com/tracks/ml5js-beginners-guide/ml5/hand-pose
 
 let video;
-let faceMesh;
-let faces = [];
-let triangles;
+let handPose;
+let hands = [];
 
 function preload() {
-  // Load FaceMesh model 
-  faceMesh = ml5.faceMesh({ maxFaces: 1, flipped: true });
+  // Initialize HandPose model with flipped video input
+  handPose = ml5.handPose({ flipped: true });
 }
 
 function mousePressed() {
-  console.log(faces);
+  console.log(hands);
 }
 
-function gotFaces(results) {
-  faces = results;
+function gotHands(results) {
+  hands = results;
 }
 
 function setup() {
@@ -25,48 +23,32 @@ function setup() {
   video = createCapture(VIDEO, { flipped: true });
   video.hide();
 
-  // Start detecting faces
-  faceMesh.detectStart(video, gotFaces);
-
-  // Get predefined triangle connections
-  triangles = faceMesh.getTriangles();
+  // Start detecting hands
+  handPose.detectStart(video, gotHands);
 }
 
 function draw() {
-  background(0);
-  video.loadPixels();
+  image(video, 0, 0);
 
-  if (faces.length > 0) {
-    let face = faces[0];
+  // Ensure at least one hand is detected
+  if (hands.length > 0) {
+    for (let hand of hands) {
+      if (hand.confidence > 0.1) {
+        // Loop through keypoints and draw circles
+        for (let i = 0; i < hand.keypoints.length; i++) {
+          let keypoint = hand.keypoints[i];
 
-    randomSeed(5);
-    beginShape(TRIANGLES);
-    
-    // Loop through each triangle and fill it with sampled pixel color
-    for (let i = 0; i < triangles.length; i++) {
-      let tri = triangles[i];
-      let [a, b, c] = tri;
-      let pointA = face.keypoints[a];
-      let pointB = face.keypoints[b];
-      let pointC = face.keypoints[c];
+          // Color-code based on left or right hand
+          if (hand.handedness == "Left") {
+            fill(255, 0, 255);
+          } else {
+            fill(255, 255, 0);
+          }
 
-      // Calculate the centroid of the triangle
-      let cx = (pointA.x + pointB.x + pointC.x) / 3;
-      let cy = (pointA.y + pointB.y + pointC.y) / 3;
-
-      // Get color from video pixels at centroid location
-      let index = (floor(cx) + floor(cy) * video.width) * 4;
-      let rr = video.pixels[index];
-      let gg = video.pixels[index + 1];
-      let bb = video.pixels[index + 2];
-
-      stroke(255, 255, 0);
-      fill(rr, gg, bb);
-      vertex(pointA.x, pointA.y);
-      vertex(pointB.x, pointB.y);
-      vertex(pointC.x, pointC.y);
+          noStroke();
+          circle(keypoint.x, keypoint.y, 16);
+        }
+      }
     }
-    
-    endShape();
   }
 }
